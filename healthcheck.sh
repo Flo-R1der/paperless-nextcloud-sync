1084d6007805d38fe6f00793442645e5a66835b0
Flo-R1der <findigutt-github@yahoo.com>
2025-12-06 17:05:39 +0100

    extend healthcheck to check if 'cron' is running

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,17 +2,25 @@
 
 # Check if Webdrive is mounted
 if findmnt -r /mnt/webdrive > /dev/null; then
-  echo "WebDAV is mounted"
+  echo "WebDAV: OK"
 else
-  echo "[ERROR] WebDAV is not mounted."
+  echo "[ERROR] WebDAV: not mounted."
   exit 1
 fi
 echo " | "
 # Check if `inotifywait` is still running
 if pgrep -f "inotifywait" > /dev/null; then
-  echo "Filewatcher is running"
+  echo "Filewatcher: OK"
 else
-  echo "[ERROR] Filewatcher is not running"
+  echo "[ERROR] Filewatcher: process not found"
+  exit 1
+fi
+echo " | "
+# Check if `cron` is still running
+if pgrep -f "cron" > /dev/null; then
+  echo "Cron: OK"
+else
+  echo "[ERROR] Cron: process not found"
   exit 1
 fi
 
