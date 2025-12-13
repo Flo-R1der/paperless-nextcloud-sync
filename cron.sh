dd1c6b534ad937ed18536660a1ea1de7e556fced
Flo-R1der <findigutt-github@yahoo.com>
2025-12-13 22:32:41 +0100

    improved cron.sh structure and logging

--- cron.sh
+++ cron.sh
@@ -6,36 +6,39 @@
 
 
 # Logging
-exec >> /var/log/cron.log
-exec 2>&1
-
 function log () {
     echo "$(date +%Y-%m-%d) $(date +%H:%M:%S) $1"
 }
+exec >> /var/log/cron.log
+exec 2>&1
+log "[INFO] Received \"$1\" Command"
 
 # Set/Load environment variables
-SOURCE_DIR="/mnt/source"
-WEBDRIVE_DIR="/mnt/webdrive"
+function setup_environment() {
+    SOURCE_DIR="/mnt/source"
+    WEBDRIVE_DIR="/mnt/webdrive"
 
-ENV_FILE="/etc/default/env"
-if [ -f "$ENV_FILE" ]; then
-    source "$ENV_FILE"
-    log "[DEBUG] Sourced env file: ${ENV_FILE:-<none>}"
-    cat "$ENV_FILE"
-else
-    log "[INFO] No Environment File found in $ENV_FILE"
-fi
+    ENV_FILE="/etc/default/env"
+    if [ -f "$ENV_FILE" ]; then
+        source "$ENV_FILE"
+        log "[DEBUG] Sourced env file: ${ENV_FILE:-<none>}"
+        cat "$ENV_FILE"
+    else
+        log "[INFO] No Environment File found in $ENV_FILE"
+    fi
+}
 
 
 # Working
 case "$1" in
     full-sync)
-        log "[ACTION] \"$1\" Command: starting a full synchronization"
-        log "[INFO] Observe container-logs for results"
+        setup_environment
+        log "[ACTION] Starting a full synchronization - Observe container-logs for results"
         /bin/bash /sync_full.sh "$SOURCE_DIR" "$WEBDRIVE_DIR" "scheduled-scan" &
         ;;
     cleanup-logs)
-        log "[ACTION] \"$1\" Command: delete Logfiles, older than $KEEP_LOGFILE_DAYS days"
+        setup_environment
+        log "[ACTION] Delete Logfiles, older than $KEEP_LOGFILE_DAYS days"
         count=$(/usr/bin/find /var/log/ -name "*.log" -type f -mtime +$KEEP_LOGFILE_DAYS | wc -l)
         log "[INFO] found $count logfiles for deletation"
         if [ $count -gt 0 ]; then
@@ -44,7 +47,7 @@ case "$1" in
         ;;
 
     *)
-        log "[ERROR] Command \"$1\" not regognized; exiting cron script"
+        log "[ERROR] Command \"$1\" unknown; exiting cron script"
         ;;
 esac
 
